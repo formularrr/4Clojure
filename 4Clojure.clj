88d6930e9c6ea2033269a27629dd387850c6c530
formularrr <447839267@qq.com>
2013-06-28 00:00:22 +0800

    4Clojure

--- 4Clojure.clj
+++ 4Clojure.clj
@@ -60,3 +60,39 @@
 ;;29
 ;;Write a function which takes a string and returns a new string containing only the capital letters.
 (#(apply str (re-seq #"[A-Z]+" %)) "SSSSsss")
+
+
+;;33
+;;Write a function which replicates each element of a sequence a variable number of times.
+((fn [x y]
+   (loop [rs '() element x]
+     (if (empty? element) rs
+           (recur (concat rs
+                          (take y (repeat (first element))))
+                  (rest element))))) [44 33] 2)
+
+
+
+;;34
+;;Write a function which creates a list of all integers in a given range.
+((fn [x y]
+   (loop [start x  rs '()]
+      (if (= start y)
+         (reverse rs)
+         (recur (inc start) (conj rs start))))) -1 4)
+
+
+;;38
+;;Write a function which takes a variable number of parameters and returns the maximum value.
+((fn [nu & x]
+   (loop [max-number nu element x]
+     (if (empty? element)
+          max-number
+         (if (< max-number (first element))
+               (recur (first element) (rest element))
+             (recur max-number (rest element)))))) 1 2 3 5 4)
+
+
+;;42
+;;Write a function which calculates factorials.
+(#(reduce * (range 1 (+ % 1))) 5)
