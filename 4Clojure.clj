2f072e1641612641a09c7cad11788c158904e10b
formula Ren <447839267@qq.com>
2013-06-28 16:23:09 +0800

    27、42、66、89、134、156

--- 4Clojure.clj
+++ 4Clojure.clj
@@ -57,6 +57,13 @@
        8)
 
 
+;;27
+;;Write a function which returns true if the given sequence is a palindrome.
+((fn [x]
+   (if (= (seq x) (reverse (seq x)))
+     true
+     false)) '(1 2 1))
+
 ;;29
 ;;Write a function which takes a string and returns a new string containing only the capital letters.
 (#(apply str (re-seq #"[A-Z]+" %)) "SSSSsss")
@@ -89,10 +96,44 @@
      (if (empty? element)
           max-number
          (if (< max-number (first element))
-               (recur (first element) (rest element))
+             (recur (first element) (rest element))
              (recur max-number (rest element)))))) 1 2 3 5 4)
 
 
 ;;42
 ;;Write a function which calculates factorials.
 (#(reduce * (range 1 (+ % 1))) 5)
+
+
+;;66
+;;Given two integers, write a function which returns the greatest common divisor.
+((fn [x y]
+   (loop [ma (max x y) mi (min x y)]
+     (if (zero? mi)
+       ma
+       (recur mi (rem ma mi))))) 1023 858)
+
+
+;;89
+;;Write a function which multiplies two numbers and returns the result as a sequence of its digits.
+((fn [x y]
+   (loop [rs (* x y) ve []]
+     (if (zero? rs)
+       ve
+       (recur (quot rs 10) (cons (rem rs 10) ve ))))) 99 9)
+
+
+;;134
+;;Write a function which, given a key and map, returns true iff the map contains an entry with that key and its value is nil.
+((fn [x se]
+   (cond (not (contains? se x)) false
+         (nil? (se x)) true
+         :else false)) :b {:a nil :b 2})
+
+;;156
+;;When retrieving values from a map, you can specify default values in case the key is not found:
+;;(= 2 (:foo {:bar 0, :baz 1} 2))
+;;However, what if you want the map itself to contain the default values? Write a function which takes a default value and a sequence of keys and constructs a map.
+((fn [ad se]
+  (apply assoc {}
+ (interleave (reverse se) (repeat ad)))) 0 [:a :b :c])
