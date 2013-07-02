a2a3131ede85faed87c4a2100fae4b5625519886
formularrr <447839267@qq.com>
2013-07-02 16:41:45 +0800

    add

--- 4Clojure.clj
+++ 4Clojure.clj
@@ -19,17 +19,17 @@
 
 ;;23
 ;;Write a function which reverses a sequence.
-
-((fn [x]
-   (loop [element x acc nil]
-     (if (empty? element)
-       acc
-       (recur (rest element) (conj acc (first element)))))) [[1 2][3 4]])
+((fn reverse-seq [n]
+   (if-not (empty? n)
+    (lazy-seq
+     (cons (last n)
+          (reverse-seq (butlast n)))))) [1 2 3 4 5])
 
 
 ;;24
 ;;Write a function which returns the sum of a sequence of numbers.
 (reduce + (list 0 -2 5 5))
+
 
 ;;25 Find the odd numbers
 ;;Write a function which returns only the odd numbers from a sequence.
@@ -194,6 +194,15 @@
  3 (range 8))
 
 
+;;55 Count Occurrences
+;;Write a function which returns a map containing the number of occurences of each distinct item in a sequence.
+((fn [coll]
+   (let [rs (reverse (group-by identity  coll))]
+     (zipmap
+       (map #(first %) rs)
+        (map #(count (second %)) rs)))) [1 1 2 3 2 1 1])
+
+
 ;;61 Map construction
 ;;Write a function which takes a vector of keys and a vector of values and constructs a map from them
 ((fn [s1 s2]
@@ -294,6 +303,7 @@
          (nil? (se x)) true
          :else false)) :b {:a nil :b 2})
 
+
 ;;135 Infix Calculator
 ;;Your friend Joe is always whining about Lisps using the prefix notation for math. Show him how you could easily write a function that does math using the infix notation. Is your favorite language that flexible, Joe? Write a function that accepts a variable length mathematical expression consisting of numbers and the operations +, -, *, and /. Assume a simple calculator that does not do precedence and instead just calculates left to right.
 ((fn [op1 & more]
@@ -317,6 +327,12 @@
 ((fn [ad se]
   (apply assoc {}
  (interleave (reverse se) (repeat ad)))) 0 [:a :b :c])
+
+
+;;157 Indexing Sequences
+;;Transform a sequence into a sequence of pairs containing the original elements along with their index.
+(#(partition 2 (interleave % (range (count %)))) [[:foo] {:bar :baz}])
+
 
 ;;166 Comparisons
 ;;For any orderable data type it's possible to derive all of the basic comparison operations (<, ≤, =, ≠, ≥, and >) from a single operation (any operator but = or ≠ will work). Write a function that takes three arguments, a less than operator for the data and two items to compare. The function should return a keyword describing the relationship between the two items. The keywords for the relationship between x and y are as follows:
